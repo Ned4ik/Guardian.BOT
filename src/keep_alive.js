eb8c2d5d0c330ec2520141f1ecba6eb910174c14
Bohdan Vozniuk <61793266+Ned4ik@users.noreply.github.com>
2023-11-22 11:25:05 +0200

    Update keep_alive.js

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.all('/', (req, res) => {
-  res.send('Your bot is alive!')
+  res.send('bot is alive!')
 
 });
 function keepAlive() {
